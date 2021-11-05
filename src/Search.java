9ec626e4c583b6876c438dad4bf095210e12d76e
Ahmed Adel <ahmed.salama758@gmail.com>
2021-11-05 17:25:53 +0200

    adding running time

--- src/Search.java
+++ src/Search.java
@@ -1,22 +1,50 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class Search {
 
     int [][] goalMat = new int[][]{{0,1,2},{3,4,5},{6,7,8}};
     Board goal = new Board(goalMat);
+    public boolean BFS(Board initialState){
+        int nodesExpanded =0;
+        Queue<Board> frontier = new LinkedList<>();
+        Set<Board> explored = new HashSet<>();
+        frontier.add(initialState);
+
+        while (!frontier.isEmpty()){
+            Board state = frontier.poll();
+            state.drawBoard();
+            explored.add(state);
+            nodesExpanded++;
+            if(goalTest(state)){
+                System.out.println("nodes expanded = " + explored.size());
+                return true;
+            }
+
+
+            for (Board neighbour : state.getNeighbours()){
+                if(!frontier.contains(neighbour) && !explored.contains(neighbour))
+                    frontier.add(neighbour);
+            }
+
+        }
+        System.out.println("nodes expanded = " + explored.size());
+        return false;
+    }
+
     public boolean depthFirstSearch(Board initialState){
         Stack<Board> frontier = new Stack<>();
         Set<Board> explored = new HashSet<>();
         frontier.push(initialState);
+        int nodesExpanded =0;
         while (!frontier.isEmpty()){
             Board state = frontier.pop();
             explored.add(state);
 //            System.out.println(explored.size());
+            nodesExpanded++;
             state.drawBoard();
 
             if(goalTest(state)){
+                System.out.println("nodes expanded = " + explored.size());
                 return true;
             }
 
@@ -25,6 +53,7 @@ public class Search {
                     frontier.push(neighbour);
             }
         }
+        System.out.println("nodes expanded = " + explored.size());
         return false;
     }
     public Boolean goalTest(Board state){
--- src/Testing.java
+++ src/Testing.java
@@ -1,12 +1,18 @@
 public class Testing {
     public static void main(String[] args) {
         int[][] arr1 = {{1,2,3},{4,0,5},{6,7,8}};
-        int[][] arr2 =  {{1,2,5},{3,4,0},{7,6,8}};
 //        int[][] arr3 = {{1,0,2},{3,4,5},{6,7,8}};
-        int[][] arr3 = {{3,1,2},{0,4,5},{6,7,8}};
+        int[][] arr3 = {{1,2,5},{3,4,0},{7,6,8}};
         Board board1 = new Board(arr1);
-        Board board2 = new Board(arr2);
+//        Board board2 = new Board(arr2);
         Board board3 = new Board(arr3);
-        System.out.println(new Search().depthFirstSearch(board2));
+        //Testing
+        long start = System.currentTimeMillis();
+        System.out.println(new Search().depthFirstSearch(board1));
+//        System.out.println(new Search().BFS(board3));
+        long end = System.currentTimeMillis();
+        long elapsedTime = end - start;
+        System.out.println("Elapsed Time: "+ elapsedTime + " ms");
     }
+
 }
