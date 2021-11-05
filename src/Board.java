50e32c9f40d168adefa371d6641a006e8f8ba23c
Ahmed Adel <ahmed.salama758@gmail.com>
2021-11-05 14:11:32 +0200

    Board class updates

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private int[][] currentState = new int[3][3];
+    private int[] zeroIndecis = new int[]{0,0};
 
+    public Board(int[][] currentState) {
+        this.currentState = currentState;
+        for(int i = 0; i< currentState.length; i++){
+            for(int j = 0; j< currentState[0].length; j++){
+                if(currentState[i][j]==0)
+                    this.zeroIndecis = new int[]{i, j};
+            }
+        }
+    }
 
+    public int[][] getCurrentState() {
+        return currentState;
+    }
+
+    public List<Board> getNeighbours() {
+        List<Board> neighbours = new ArrayList<>();
+        // swap zero with each of its available neighbours to get a new state
+        int zeroX = zeroIndecis[0], zeroY = zeroIndecis[1];
+        if(zeroX>0){
+            int tempValue = currentState[zeroX-1][zeroY];
+            neighbours.add(generateNewState(tempValue, zeroX-1, zeroY));
+        }
+        if(zeroX<2){
+            int tempValue = currentState[zeroX+1][zeroY];
+            neighbours.add(generateNewState(tempValue, zeroX+1, zeroY));
+        }
+        if(zeroY>0){
+            int tempValue = currentState[zeroX][zeroY-1];
+            neighbours.add(generateNewState(tempValue, zeroX, zeroY-1));
+        }
+        if(zeroY<2){
+            int tempValue = currentState[zeroX][zeroY+1];
+            neighbours.add(generateNewState(tempValue, zeroX, zeroY+1));
+        }
+        return neighbours;
+    }
+    private Board generateNewState(int tempValue, int xIdx, int yIdx){
+        int[][] newState = new int[3][3];
+        for(int i = 0; i< currentState.length; i++){
+            for(int j = 0; j< currentState[0].length; j++){
+                if(currentState[i][j]==0)
+                    newState[i][j] = 0;
+                else if(i==xIdx && j==yIdx)
+                    newState[i][j] = tempValue;
+                else
+                    newState[i][j] = currentState[i][j];
+            }
+        }
+        return new Board(newState);
+    }
 }
--- src/Search.java
+++ src/Search.java
@@ -1,9 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 public class Search {
 
 
 
+    public boolean depthFirstSearch(Board initialState){
+        Stack<Board> frontier = new Stack<>();
+        Set<Board> explored = new HashSet<>();
+        while (!frontier.isEmpty()){
+            Board state = frontier.pop();
+            explored.add(state);
 
-    public Boolean goalTest(){
+            if(goalTest(state)){
+                return true;
+            }
+            for(Board neighbour: state.getNeighbours()) {
+
+            }
+        }
+        return false;
+    }
+    public Boolean goalTest(Board state){
         return false;
 
     }
